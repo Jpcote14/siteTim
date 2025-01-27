0aa470ca672441a9a3efcde5214b3e9cd21bf342
Jpcote14 <jean-philippe_cote@hotmail.com>
2025-01-27 18:50:11 -0500

    ajout de css pour le bouton

--- ressources/vues/pages/nousJoindre.blade.php
+++ ressources/vues/pages/nousJoindre.blade.php
@@ -1,61 +1,61 @@
 @extends('gabarit')
 
 @section('title')
-    Nous joindre
+Nous joindre
 @endsection
 {{-- <div>{{ $message->getTelephone() }}</div> --}}
 @section('contenu')
-    <h1>Nous joindre</h1>
-        
-            @foreach($responsables as $responsable)
-                <div>
-{{-- ajouter une image de chaque prof --}}
-                    {{-- <img src="" alt=""> --}}
-<p>{{$responsable->getPrenom()}} {{$responsable->getNom()}}</p>
-<p>{{$responsable->getResponsabilite()}}</p>
-<p>{{$responsable->getTelephone()}}</p>
+<h1>Nous joindre</h1>
 
-                </div>
+@foreach($responsables as $responsable)
+<div>
+    {{-- ajouter une image de chaque prof --}}
+    {{-- <img src="" alt=""> --}}
+    <p>{{$responsable->getPrenom()}} {{$responsable->getNom()}}</p>
+    <p>{{$responsable->getResponsabilite()}}</p>
+    <p>{{$responsable->getTelephone()}}</p>
 
-                <br>
-            @endforeach
-        
-        
+</div>
 
+<br>
+@endforeach
+
+
+<div class="div-gradient">test</div>
 {{-- Ajouter une action pour faire fonctionner le formulaire --}}
 <form action="#" method="post">
 
-<h2>Contacte par téléphone</h2>
+    <h2>Contacte par téléphone</h2>
 
-<div class="ctn-input">
-    <label class="" for="joindrePrenom">Prénom</label>
-    <input
-        type="text"
-        id="joindrePrenom"
-        name=""
-        placeholder=""
-        aria-labelledby=""
-        aria-required="true"
-        aria-invalid="false"
-        aria-describedby=""
-        value=""
-        class="input" />
-</div>
+    <div class="ctn-input">
+        <label class="" for="joindrePrenom">Prénom</label>
+        <input
+            type="text"
+            id="joindrePrenom"
+            name=""
+            placeholder=""
+            aria-labelledby=""
+            aria-required="true"
+            aria-invalid="false"
+            aria-describedby=""
+            value=""
+            class="input" />
+    </div>
 
-<div class="ctn-input">
-    <label class="" for="joindreNom">Nom</label>
-    <input
-        type="text"
-        id="joindreNom"
-        name=""
-        placeholder=""
-        aria-labelledby=""
-        aria-required="true"
-        aria-invalid="false"
-        aria-describedby=""
-        value=""
-        class="input" />
-</div>
+    <div class="ctn-input">
+        <label class="" for="joindreNom">Nom</label>
+        <input
+            type="text"
+            id="joindreNom"
+            name=""
+            placeholder=""
+            aria-labelledby=""
+            aria-required="true"
+            aria-invalid="false"
+            aria-describedby=""
+            value=""
+            class="input" />
+    </div>
 
 
 
@@ -66,4 +66,3 @@
 
 
 @endsection
-
